a6177a0002537f969ec83c00e330add4b3e6d352
cannorin <cannorin@users.noreply.github.com>
2022-07-31 04:51:42 +0900

    Initial commit

---
+++ Lib.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using Rewired;
+using UnityEngine;
+
+namespace Patches
+{
+    [HarmonyPatch(typeof(Player))]
+    [HarmonyPatch("Update")]
+    class Player_Update
+    {
+        private static float nextTime = 0.0f;
+        private static float interval = 0.1f;
+
+        static void Postfix(Player __instance)
+        {
+            var p = __instance;
+            var rewiredPlayer = p.rewiredPlayer;
+            if (p.controlBlocks.Count < 1 && rewiredPlayer != null && !ControllerDisconnectCtrl.controllerDisconnectInProgress && !UIInputFieldVirtualKeyboard.inputCaptureInProgress)
+            {
+                var time = Time.time;
+                if (rewiredPlayer.GetButtonTimedPress("FireOne", 0.5f) && time > nextTime)
+                {
+                    nextTime = time + interval;
+                    p.QueueAction(InputAction.FireOne);
+                }
+                else if (rewiredPlayer.GetButtonTimedPress("FireTwo", 0.5f) && Time.time > nextTime)
+                {
+                    nextTime = time + interval;
+                    p.QueueAction(InputAction.FireTwo);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Full-Auto Spell Mod for One Step From Eden
+==========================================
+
+Available on [Steam Workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2842168759).
+
+# License
+
+Apache 2.0
